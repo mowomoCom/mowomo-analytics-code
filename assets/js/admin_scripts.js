44aefdf1c235ea4760baaca676c44e6fcdb9246f
Victor <victor@mowomo.com>
2020-08-06 14:20:46 +0200

    nueva version de administrador

--- assets/js/admin_scripts.js
+++ assets/js/admin_scripts.js
@@ -1,3 +1,50 @@
 jQuery(document).ready(function() {
-    
+	
+	jQuery("#generar-link").on("click", function() {
+		
+		jQuery.ajax({
+			url: mwm_ana_admin_data.ajax_url,
+			type: "post",
+			data: {
+				action: "mwm_get_token"
+			},
+
+			beforeSend: function() {},
+			
+			success: function(response) {
+				// Get data
+				let data = JSON.parse(response);
+				console.log(data);
+				jQuery("#cooler_intruder_url").html(data);
+			}
+
+	  });
+
+	});
+
+	jQuery("#guardar_cambios").on("click", function() {
+
+		let mwm_ac_ID_propiedad = jQuery("#mwm_ac_ID_propiedad").val();
+		
+		jQuery.ajax({
+			url: mwm_ana_admin_data.ajax_url,
+			type: "post",
+			data: {
+				action: "mwm_ana_save_data",
+				mwm_ac_ID_propiedad: mwm_ac_ID_propiedad
+			},
+
+			beforeSend: function() {},
+			
+			success: function(response) {
+				// Get data
+				let data = JSON.parse(response);
+				jQuery("#alertas").html(data);
+			}
+
+		});
+
+	});
+
 });
+  
--- includes/admin/admin-configuration.php
+++ includes/admin/admin-configuration.php
@@ -1,41 +1,16 @@
 <?php
 
 /**
- * Genera un token numérico para la url de acceso cuando está en mantenimiento.
- *
- * @since      1.0.0
- */
-function mwm_get_token() {
-
-    $token = substr( sha1( strval( rand( 1 , 9999 ) ) ) , 0 , 6 );
-    update_option( 'mwm_ana_access_token' , $token );
-	echo json_encode( array( get_site_url() . "?tk=" . $token ) );
-	wp_die();
-
-}
-
-/**
  * Guarda las configuraciones de la página de configuración del plugin.
  *
  * @since      1.0.0
  */
 function mwm_ana_save_data() {
 
-    $activo = $_POST[ 'activo' ];
-	$email_contacto = $_POST[ 'email_contacto' ];
-
-    if ( $activo == "true" ) {
-
-        update_option( 'mwm_ana_activo' , 'activo' );
-
-	} else {
-
-        update_option( 'mwm_ana_activo' , 'desactivado' );
-
-	}
-
-	update_option('email_contacto',$email_contacto);
-    echo json_encode(array('Cambios realizados con exito')); wp_die();
+	$mwm_ac_ID_propiedad = $_POST[ 'mwm_ac_ID_propiedad' ];
+	update_option('mwm_ac_ID_propiedad',$mwm_ac_ID_propiedad);
+    echo json_encode(array('Cambios realizados con exito')); 
+    wp_die();
 
 }
 
@@ -54,7 +29,7 @@ add_action('wp_ajax_nopriv_mwm_ana_save_data', 'mwm_ana_save_data');
 add_action('admin_menu', 'mwm_ana_paginas');
 
 function mwm_ana_paginas() {
-    add_menu_page( 'mowomo page', 'Mantenimiento', 'manage_options', 'mwm-man', 'mwm_ana_page', plugin_dir_url( __FILE__ ).'/assets/logo-mowomo-white.svg' );
+    add_menu_page( 'mowomo page', 'Analytics Code', 'manage_options', 'mwm-man', 'mwm_ana_page', mwm_ana_IMG.'logo-mowomo-white.svg' );
 }
 
 /**
@@ -72,22 +47,20 @@ function mwm_ana_page() {
         
             
 
-            <h1><?php _e('mowomo Mantenimiento', 'mwm-man'); ?></h1>
+            <h1><?php _e('mowomo Analytics Code', 'mwm-man'); ?></h1>
 
             <h2 class="nav-tab-wrapper">
                 <a href="javascript:void(0);" mwm-tab="mwm_ana_conf" class="nav-tab mwm_ana_conf"><?php _e( 'Configuración general', 'mwm_ana' ); ?></a>
             </h2>
 
             <!-- Dinamic Admin Form -->
-            <form id="mwm-admin-form" mwm-current-tab="mwm_ana_conf" mwm-page-slug="mwm-man" action="options.php" method="post">
-
                 <div id="tab-mwm_ana_conf" class="mwm-tab">
                     <table class="form-table">
                         <label id="alertas"></label>
                         <tbody>
                         <tr>
                             <td>
-                                <input type="text" name="mwm_ac_ID_propiedad" id="mwm_ac_ID_propiedad" value="<?php echo get_option('mwm_ac_ID_propiedad'); ?>">
+                                <input type="text" name="mwm_ac_ID_propiedad" id="mwm_ac_ID_propiedad" value="<?php echo get_option('mwm_ac_ID_propiedad    '); ?>">
                                 <p class="description"><?php _e('User ID, same that UA-XXXXXXXXX-X, you can get the code','mwm_ac'); ?></p><a href="https://support.google.com/analytics/answer/1008080?hl=en"><?php _e('in this article','mwm_ac');?></a>
                             </td>
                         </tr>
@@ -98,8 +71,7 @@ function mwm_ana_page() {
                     
                 </div>
 
-                <p><?php @submit_button(); ?></p>
-            </form>
+                <button id="guardar_cambios"><?php _e( 'Guardar cambios' , 'mwm-man' ); ?></button>
         </div>
 
         <?php 
